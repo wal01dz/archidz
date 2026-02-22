2ca3c30eaf77f10c65913cade1e2062622761c91
wal01dz <wal01dz@gmail.com>
2026-02-22 09:40:56 +0100

    Fix ChatProjet props

--- components/ChatProjet.tsx
+++ components/ChatProjet.tsx
@@ -1,1 +1,14 @@
-export default function ChatProjet() { return null; }
+"use client";
+
+interface Props {
+  projetId: string;
+  otherUser: {
+    id: string;
+    name: string;
+    avatar: string | null;
+  };
+}
+
+export default function ChatProjet({ projetId, otherUser }: Props) {
+  return null;
+}
