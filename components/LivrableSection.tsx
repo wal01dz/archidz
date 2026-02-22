9cb3c0b68ec089aa1970d47535d913efe0b29f82
wal01dz <wal01dz@gmail.com>
2026-02-22 09:38:01 +0100

    Fix LivrableSection props

--- components/LivrableSection.tsx
+++ components/LivrableSection.tsx
@@ -1,1 +1,14 @@
-export default function LivrableSection() { return null; }
+"use client";
+
+interface Props {
+  projetId: string;
+  statut: string;
+  livrables: string[];
+  isClient: boolean;
+  isFreelance: boolean;
+  hasAvis: boolean;
+}
+
+export default function LivrableSection({ projetId, statut, livrables, isClient, isFreelance, hasAvis }: Props) {
+  return null;
+}
